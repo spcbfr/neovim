de9291080a57ba2b1ec7fbdc8c0811e023595ef5
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2024-07-16 05:05:31 +0100

    Remove unnecessary plugins

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -7,23 +7,7 @@ return {
   cmd = 'Grapple',
   keys = {
     { '<leader>m', '<cmd>Grapple toggle<cr>', desc = 'Grapple toggle tag' },
-    {
-      '<leader>M',
-      function()
-        local Oil = require 'oil'
-        local filename = Oil.get_cursor_entry().name
-        local directory = Oil.get_current_dir()
-
-        local Grapple = require 'grapple'
-        local Path = require 'grapple.path'
-        Grapple.toggle { path = Path.join(directory, filename) }
-      end,
-      desc = 'Grapple tag under cursor',
-    },
     { '<leader>k', '<cmd>Grapple toggle_tags<cr>', desc = 'Grapple toggle tags' },
-    { '<leader>K', '<cmd>Grapple toggle_scopes<cr>', desc = 'Grapple toggle scopes' },
-    { '<leader>j', '<cmd>Grapple cycle forward<cr>', desc = 'Grapple cycle forward' },
-    { '<leader>J', '<cmd>Grapple cycle backward<cr>', desc = 'Grapple cycle backward' },
     { '<leader>1', '<cmd>Grapple select index=1<cr>', desc = 'Grapple select 1' },
     { '<leader>2', '<cmd>Grapple select index=2<cr>', desc = 'Grapple select 2' },
     { '<leader>3', '<cmd>Grapple select index=3<cr>', desc = 'Grapple select 3' },
